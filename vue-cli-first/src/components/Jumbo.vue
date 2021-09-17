be2919867e8f600d34027b175131fab3fd45b364
Stefano <stefano.funaro@gmail.com>
2021-09-17 16:09:37 +0200

    esercizio+bonus

---
+++ vue-cli-first/src/components/Jumbo.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <img src="../assets/batman.jpg">
+    </div>
+</template>
+<script>
+export default {
+    name : 'Jumbo',
+    
+}
+</script>
+<style scoped>
+    div {
+        display: flex;
+        justify-content: center;
+    }
+</style>
---
+++ vue-cli-first/src/components/Title.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <p>
+            {{msg}}
+        </p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Title',
+    props: {
+        msg:String,
+    }
+}
+</script>
+<style scoped>
+div {
+    font-size: 2rem;
+    color: red;
+    left: 50%;
+    width: 50vw;
+    background-color: blue;
+    height: 20vh;
+    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
